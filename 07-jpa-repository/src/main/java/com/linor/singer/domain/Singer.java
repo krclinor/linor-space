f1a5e2a831734136e8d97c8bc4b9b93ee825b8b1
krclinor <krclinor@gmail.com>
2019-04-07 11:57:44 +0900

    CrudRepository를 이용한 개발

---
+++ 07-jpa-repository/src/main/java/com/linor/singer/domain/Singer.java
@@ -0,0 +1,76 @@
+package com.linor.singer.domain;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+//@Table(name="singer")
+@Data
+@NamedQueries({
+	@NamedQuery(name="Singer.findById",
+			query="select distinct s from Singer s " +
+			"left join fetch s.albums a " +
+			"left join fetch s.instruments i " +
+			"where s.id = :id"),
+	@NamedQuery(name="Singer.findAllWithAlbum",
+			query="select distinct s from Singer s \n"
+					+ "left join fetch s.albums a \n"
+					+ "left join fetch s.instruments i")
+})
+public class Singer implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	//@Column(name="first_name")
+	private String firstName;
+	
+	//@Column(name="last_name")
+	private String lastName;
+	
+	//@Column(name="birth_date")
+	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy="singer", cascade=CascadeType.ALL, orphanRemoval=true, fetch=FetchType.EAGER)
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private Set<Album> albums = new HashSet<>();
+
+	@ManyToMany
+	@JoinTable(name="singer_instrument", 
+		joinColumns=@JoinColumn(name="singer_id"),
+		inverseJoinColumns=@JoinColumn(name="instrument_id"))
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private Set<Instrument> instruments = new HashSet<>();
+	
+	@Version
+	private int version;
+	
+	public boolean addAlbum(Album album) {
+		album.setSinger(this);
+		return getAlbums().add(album);
+	}
+	public void reoveAlbum(Album album) {
+		getAlbums().remove(album);
+	}
+}
