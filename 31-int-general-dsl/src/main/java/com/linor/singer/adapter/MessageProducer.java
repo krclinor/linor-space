0ea6834f5a0b7093dd83266e5557acb05b09260c
krclinor <krclinor@gmail.com>
2019-04-13 11:32:35 +0900

    스프링 Integration DSL 방식

--- 31-int-general-dsl/src/main/java/com/linor/singer/adapter/MessageProducer.java
+++ 31-int-general-dsl/src/main/java/com/linor/singer/adapter/MessageProducer.java
@@ -2,21 +2,12 @@ package com.linor.singer.adapter;
 
 import java.util.Random;
 
-import org.springframework.integration.core.MessageSource;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MessageProducer implements MessageSource<String> {
-
-	private String produce() {
+public class MessageProducer{
+	public String produce() {
 		String[] array = {"첫 번째 줄!", "두 번째 줄!", "세 번째 줄!"};
 		return array[new Random().nextInt(3)];
-	}
-
-	@Override
-	public Message<String> receive() {
-		return new GenericMessage<String>(this.produce());
 	}
 }
--- 31-int-general-dsl/src/main/java/com/linor/singer/config/IntegrationFlowConfig.java
+++ 31-int-general-dsl/src/main/java/com/linor/singer/config/IntegrationFlowConfig.java
@@ -1,7 +1,5 @@
 package com.linor.singer.config;
 
-import java.util.Random;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.config.EnableIntegration;
@@ -9,8 +7,7 @@ import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.GenericMessage;
+import org.springframework.integration.endpoint.MethodInvokingMessageSource;
 
 import com.linor.singer.adapter.MessageConsumer;
 import com.linor.singer.adapter.MessageProducer;
@@ -18,25 +15,18 @@ import com.linor.singer.adapter.MessageProducer;
 @Configuration
 @EnableIntegration
 public class IntegrationFlowConfig {
-//	@Bean
-//	public MessageSource<String> source(){
-//		return new MessageSource<String>() {
-//			@Override
-//			public Message<String> receive() {
-//				String[] array = {"첫 번째 줄!", "두 번째 줄!", "세 번째 줄!"};
-//				return new GenericMessage<String>(array[new Random().nextInt(3)]);
-//			}
-//		};
-//	}
+
+	@Bean
+	public MessageSource<?> source(){
+		MethodInvokingMessageSource source = new MethodInvokingMessageSource();
+		source.setObject(new MessageProducer());
+		source.setMethodName("produce");
+		return source;
+	}
 
 	@Bean
 	public IntegrationFlow inOutFlow(MessageProducer messageProducer, MessageConsumer messgeConsumer) {
-//		return IntegrationFlows.from(source(), s -> s.poller(Pollers.fixedDelay(1000)))
-//				.channel("channel")
-//				.handle(messgeConsumer, "consume")
-//				.get();
-
-		return IntegrationFlows.from(messageProducer, s -> s.poller(Pollers.fixedDelay(1000)))
+		return IntegrationFlows.from(source(), s -> s.poller(Pollers.fixedDelay(1000)))
 				.channel("channel")
 				.handle(messgeConsumer, "consume")
 				.get();
