ca500d5b91a9fc22431c6fa12f38a666eed65028
krclinor <krclinor@gmail.com>
2019-04-22 10:54:12 +0900

    WebJar

---
+++ 02-04-mvc-webjar/src/main/java/com/linor/singer/config/ControllersConfig.java
@@ -0,0 +1,22 @@
+package com.linor.singer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ControllersConfig  implements WebMvcConfigurer{
+
+//	@Override
+//	public void configureViewResolvers(ViewResolverRegistry registry) {
+//		registry.jsp("/WEB-INF/jsp/", ".jsp");
+//	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/hello").setViewName("hello");
+	}
+	
+}
