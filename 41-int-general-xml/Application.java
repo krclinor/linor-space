42ed3efea8b6ad03196494a4126fdcad0041d0ec
krclinor <krclinor@gmail.com>
2019-04-22 10:24:25 +0900

    MVC Static Contents

---
+++ 41-int-general-xml/Application.java
@@ -0,0 +1,17 @@
+package com.linor.singer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+@ImportResource("classpath:schedule-context.xml")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
