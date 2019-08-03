f7164110e263c065ef46854fed3c559dd63ee9cc
linor <linor@linor>
2019-08-03 14:40:31 +0900

    mvc filedownload

--- 02-08-mvc-filedownload/src/main/java/com/linor/singer/controller/FileController.java
+++ 02-08-mvc-filedownload/src/main/java/com/linor/singer/controller/FileController.java
@@ -20,11 +20,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
