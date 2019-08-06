09599f799fec57ba92b948719fac472ebc548b12
linor <linor@linor>
2019-08-06 12:22:32 +0900

    jdbc

--- 01-02-jdbc-jdbc/src/main/java/com/linor/singer/JdbcApplication.java
+++ 01-02-jdbc-jdbc/src/main/java/com/linor/singer/JdbcApplication.java
@@ -1,13 +1,24 @@
 package com.linor.singer;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JdbcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
+	}
+
+	@Bean
+	public DataSourceTransactionManager txManager1(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
 	}
 
 }
--- 01-02-jdbc-jdbc/src/test/java/com/linor/singer/SingerDaoTests.java
+++ 01-02-jdbc-jdbc/src/test/java/com/linor/singer/SingerDaoTests.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.linor.singer.dao.SingerDao;
 import com.linor.singer.domain.Album;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 @Slf4j
 public class SingerDaoTests {
 	@Autowired
