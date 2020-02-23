0023ae20d2eb058a679062a68613492061a856ed
krclinor <krclinor@gmail.com>
2020-02-23 12:16:09 +0900

    Actuator

--- 05-01-manage-actuator/src/main/java/com/linor/singer/Application.java
+++ 05-01-manage-actuator/src/main/java/com/linor/singer/Application.java
@@ -1,4 +1,4 @@
-package com.linor.ioc;
+package com.linor.singer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 05-01-manage-actuator/src/test/java/com/linor/singer/ApplicationTests.java
+++ 05-01-manage-actuator/src/test/java/com/linor/singer/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.linor.ioc;
+package com.linor.singer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
