fa60dcd33604a784e0026d86d4e7802db7707f3b
krclinor <krclinor@gmail.com>
2020-03-01 10:31:09 +0900

    Actuator

---
+++ 05-01-manage-actuator/src/main/java/com/linor/singer/component/FeedServerHealthIndicator.java
@@ -0,0 +1,36 @@
+package com.linor.singer.component;
+
+import java.util.Date;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class FeedServerHealthIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://feedserver.com/ping";
+		try {
+			String resp = restTemplate.getForObject(url, String.class);
+			if("OK".equalsIgnoreCase(resp)) {
+				return Health.up()
+						.build();
+			}else {
+				return Health.down()
+						.withDetail("ping_url", url)
+						.withDetail("ping_time", new Date())
+						.build();
+			}
+		}catch(RestClientException e) {
+			return Health.down(e)
+					.withDetail("ping_url", url)
+					.withDetail("ping_time", new Date())
+					.build();
+		}
+	}
+}
