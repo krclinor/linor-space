df050fd2133f52b78bfb9447438f43f25a6acd92
krclinor <krclinor@gmail.com>
2020-05-15 21:54:36 +0900

    연습용 초기 모델

--- 01-01-jdbc-todo/src/main/java/com/linor/singer/dao/SingerDao.java
+++ 01-01-jdbc-todo/src/main/java/com/linor/singer/dao/SingerDao.java
@@ -2,17 +2,33 @@ package com.linor.singer.dao;
 
 import java.util.List;
 
+import com.linor.singer.domain.Album;
+import com.linor.singer.domain.Instrument;
 import com.linor.singer.domain.Singer;
+import com.linor.singer.domain.SingerSummary;
 
 public interface SingerDao {
 	List<Singer> findAll();
+	List<Singer> findAllByNativeQuery();
+
 	List<Singer> findByFirstName(String firstName);
+	List<Singer> findByFirstNameAndLastName(Singer singer);
+	List<Singer> findAllWithAlbums();
+
+	List<Album> findAlbumsBySinger(Singer singer);
+	List<Album> findAlbumsByTitle(String title);
+
 	String findNameById(Integer id);
-	Singer findById(Integer id);
 	String findFirstNameById(Integer id);
+
+	Singer findById(Integer id);
 	void insert(Singer singer);
 	void update(Singer singer);
 	void delete(Integer singerId);
-	List<Singer> findAllWithAlbums();
+	
 	void insertWithAlbum(Singer singer);
+
+	void insertInstrument(Instrument instrument);
+
+	public List<SingerSummary> listAllSingersSummary();
 }
--- 01-01-jdbc-todo/src/main/java/com/linor/singer/domain/Singer.java
+++ 01-01-jdbc-todo/src/main/java/com/linor/singer/domain/Singer.java
@@ -1,7 +1,7 @@
 package com.linor.singer.domain;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,5 +18,6 @@ public class Singer {
 	private String lastName;
 	private LocalDate birthDate;
 	
-	private List<Album> albums;
+	private Set<Album> albums;
+	private Set<Instrument> instruments;
 }
