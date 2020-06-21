ec3c568ba32697e2be2e3b1ea0878aab4393ed05
krclinor <krclinor@gmail.com>
2020-06-21 12:20:34 +0900

    삭제

--- 03-02-rest-controller-jpa/src/main/java/com/linor/singer/repository/InstrumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.linor.singer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.linor.singer.domain.Instrument;
-
-public interface InstrumentRepository extends JpaRepository<Instrument, String> {
-
-}
