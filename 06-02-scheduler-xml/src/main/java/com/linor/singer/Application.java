7c0288a9aaf092f3a8841811af8b9bfed7767314
krclinor <krclinor@gmail.com>
2020-07-25 11:17:26 +0900

    Scheduler XML

--- 06-02-scheduler-xml/src/main/java/com/linor/singer/Application.java
+++ 06-02-scheduler-xml/src/main/java/com/linor/singer/Application.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("classpath:schedule-context.xml")
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
